99404832464ff33eaa50424483e2844a2111f7b9
artsimon <simona1@sfusd.edu>
2015-10-19 13:13:49 -0700

    initial commit

---
+++ Thingy.java
@@ -0,0 +1,12 @@
+class Thingy
+{
+  private int myX, myY;
+  public Thingy()
+  {
+    myX = myY = 0;
+  }
+  public int getX(){/* your java code */}
+  public int getY(){/* your java code */}
+  public void setX(int x){/* your java code */}
+  public void setY(int y){/* your java code */}
+}
