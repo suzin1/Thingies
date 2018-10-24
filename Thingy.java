c4b5578d3827d053aa4003aefd86abeb7087cb3d
suzin <suzin@s.sfusd.edu>
2018-10-24 14:01:16 -0700

    1

--- Thingy.java
+++ Thingy.java
@@ -6,4 +6,8 @@ class Thingy
     myX = myY = 0;
   }
   /* your java code for "getters" and "setters" here */
+  public int getX(){return myX;}
+  public void setX(int x){myX = x;}
+  public int getY(){return myY;}
+  public void setY(int y){myY = y;}
 }
